38c2013be1c167a6c8e99038afd38a4701102c51
Ritesh Kumar <riteshk6341@gmail.com>
2020-02-13 19:34:05 +0530

    first commit

---
+++ SpringCore/src/main/java/com/btm/jspider/SpringCore/Employee.java
@@ -0,0 +1,41 @@
+package com.btm.jspider.SpringCore;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private long sal;
+	private String loc;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getSal() {
+		return sal;
+	}
+
+	public void setSal(long sal) {
+		this.sal = sal;
+	}
+
+	public String getLoc() {
+		return loc;
+	}
+
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+}
