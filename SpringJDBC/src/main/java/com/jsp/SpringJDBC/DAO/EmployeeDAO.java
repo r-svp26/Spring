f83225fd283e7218b0bf0143d1ef920191e141bf
Ritesh Kumar <riteshk6341@gmail.com>
2020-10-31 20:19:38 +0530

    jdbc

---
+++ SpringJDBC/src/main/java/com/jsp/SpringJDBC/DAO/EmployeeDAO.java
@@ -0,0 +1,13 @@
+package com.jsp.SpringJDBC.DAO;
+
+import com.jsp.SpringJDBC.entity.Employee;
+
+public interface EmployeeDAO {
+
+	int create(Employee emp);
+
+	int update(Employee emp);
+
+	int delete(int id);
+
+}
